6e258d5e9e802ac62e4bed6f78ddf198c810b05f
WeiXiang123 <jerry@tootootekiMacBook-Air.local>
2015-01-27 15:16:13 +0800

    Initial Commit

---
+++ TableViewSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewSample
+//
+//  Created by WeiXiang on 15/1/27.
+//  Copyright (c) 2015年 WeiXiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableViewSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TableViewSample
+//
+//  Created by WeiXiang on 15/1/27.
+//  Copyright (c) 2015年 WeiXiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
